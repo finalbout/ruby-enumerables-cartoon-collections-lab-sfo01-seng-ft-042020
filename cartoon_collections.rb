8e4f473020497a81b68ff1843babbd16fca4dda7
John Tom Jr <johntjr.22@gmail.com>
2020-03-14 04:00:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ i = 0
   puts "#{i + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array
 end
 
 def long_planeteer_calls# code an argument here
